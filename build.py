1dcb38f83420bf5d7b8e3209a498838e15e1d3da
Daniil10295 <mnc0682@gmail.com>
2023-12-03 19:35:59 +0300

    update build.py

--- build.py
+++ build.py
@@ -1,19 +1,28 @@
 from jinja2 import Environment, FileSystemLoader, Template, select_autoescape
 import os
+import github
 
-env = Environment(
-    loader=FileSystemLoader('templates'),
+env = Environment(loader=FileSystemLoader('templates'),
     autoescape=select_autoescape(['html', 'xml'])
 )
 
+gh = github.Github()
+
 info = {
-    'projects': ['test1', 'test2', 'test3'],
-    'projects_urls': {
-        'test1': 'test1',
-        'test2': 'test2',
-        'test3': 'test3'
-    }
+    'projects': [], 'projects_urls': {}
 }
+
+def list_of_projects() -> list:
+    repos = gh.get_organization("RandNameOfOrg").get_repos()
+    non_forks = []
+    for repo in repos:
+        if repo.fork is False:
+            non_forks.append(repo.name)
+    return non_forks
+
+for project in list_of_projects():
+    info['projects'].append(project); 
+    info['projects_urls'][project] = "/" + project
 
 for file in os.listdir('build_output'):
     os.remove(f'build_output\\{file}')
