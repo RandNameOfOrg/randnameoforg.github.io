39b0929595f9bc5085034f939cce8ab62ab0f53b
Daniil10295 <151400837+Daniil10295@users.noreply.github.com>
2023-12-04 22:09:44 +0300

    update build

--- build.py
+++ build.py
@@ -2,7 +2,7 @@ from jinja2 import Environment, FileSystemLoader, Template, select_autoescape
 import os
 import github
 
-output_folder = '/output/'
+output_folder = 'output'
 
 env = Environment(loader=FileSystemLoader('templates'),
     autoescape=select_autoescape(['html', 'xml'])
@@ -30,9 +30,9 @@ if not os.path.exists(output_folder):
     print('Creating output folder...')
     os.mkdir(output_folder)
 else:
-    for file in os.listdir('build_output'):
-        os.remove(f'{output_folder}{file}')
+    for file in os.listdir(f'{output_folder}\\'):
+        os.remove(f'{output_folder}\\{file}')
 
 for _, _, files in os.walk('templates'):
     for file in files: 
-        env.get_template(file).stream(info=info).dump(f'{output_folder}{file}')
+        env.get_template(file).stream(info=info).dump(f'{output_folder}\\{file}')
